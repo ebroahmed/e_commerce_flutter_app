597481411e5c8dec61916244a6da3c77ba1ffcc0
Ebrahim <ebrahimahmed804853@gmail.com>
2025-08-02 18:09:29 +0300

    product detail screen UI updated

---
+++ lib/screens/product_details_screen.dart
@@ -0,0 +1,147 @@
+// lib/screens/product_details_screen.dart
+import 'package:e_commerce_flutter_app/models/product_model.dart';
+import 'package:e_commerce_flutter_app/providers/cart_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ProductDetailsScreen extends ConsumerStatefulWidget {
+  const ProductDetailsScreen({super.key, required this.product});
+  final Product product;
+
+  @override
+  ConsumerState<ProductDetailsScreen> createState() =>
+      _ProductDetailsScreenState();
+}
+
+class _ProductDetailsScreenState extends ConsumerState<ProductDetailsScreen> {
+  int quantity = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    final product = widget.product;
+
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Theme.of(context).colorScheme.surface),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text(
+          product.name,
+          style: TextStyle(color: Theme.of(context).colorScheme.surface),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //  Product Image
+            AspectRatio(
+              aspectRatio: 1,
+              child: Image.network(product.imageUrl, fit: BoxFit.contain),
+            ),
+
+            //  Product Info
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    product.name,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    '\$${product.price.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: const Color.fromARGB(255, 119, 108, 6),
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  Text(
+                    product.description,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontSize: 15,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+
+                  // Quantity Selector
+                  Row(
+                    children: [
+                      Text(
+                        'Quantity:',
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      IconButton(
+                        icon: Icon(
+                          Icons.remove,
+                          color: const Color.fromARGB(255, 133, 30, 22),
+                          size: 20,
+                        ),
+                        onPressed: quantity > 1
+                            ? () => setState(() => quantity--)
+                            : null,
+                      ),
+                      Text(quantity.toString(), style: TextStyle(fontSize: 20)),
+                      IconButton(
+                        icon: Icon(
+                          Icons.add,
+                          size: 20,
+                          color: const Color.fromARGB(255, 15, 119, 18),
+                        ),
+                        onPressed: () => setState(() => quantity++),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+
+                  // Add to Cart Button
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton.icon(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Theme.of(context).colorScheme.surface,
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                      ),
+                      onPressed: () {
+                        ref
+                            .read(cartProvider.notifier)
+                            .addToCart(product, quantity);
+
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            backgroundColor: Theme.of(
+                              context,
+                            ).colorScheme.primary,
+                            content: Text(
+                              'Added ${product.name} to cart',
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.surface,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      icon: Icon(Icons.shopping_cart),
+                      label: Text('Add to Cart'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -1,56 +1,66 @@
 import 'package:e_commerce_flutter_app/models/product_model.dart';
+import 'package:e_commerce_flutter_app/screens/product_details_screen.dart';
 import 'package:flutter/material.dart';
 
 class ProductCard extends StatelessWidget {
-  final Product product;
-
   const ProductCard({super.key, required this.product});
+  final Product product;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 4,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          //  Product Image
-          Expanded(
-            child: ClipRRect(
-              borderRadius: const BorderRadius.vertical(
-                top: Radius.circular(10),
-              ),
-              child: Image.network(
-                product.imageUrl,
-                width: double.infinity,
-                fit: BoxFit.contain,
-                errorBuilder: (_, __, ___) =>
-                    const Center(child: Icon(Icons.broken_image)),
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (ctx) => ProductDetailsScreen(product: product),
+          ),
+        );
+      },
+      child: Card(
+        color: Colors.white,
+        elevation: 4,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //  Product Image
+            Expanded(
+              child: ClipRRect(
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(10),
+                ),
+                child: Image.network(
+                  product.imageUrl,
+                  width: double.infinity,
+                  fit: BoxFit.contain,
+                  errorBuilder: (_, __, ___) =>
+                      const Center(child: Icon(Icons.broken_image)),
+                ),
               ),
             ),
-          ),
 
-          //  Product Info
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              product.name,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Theme.of(context).colorScheme.primary,
+            //  Product Info
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                product.name,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8.0),
-            child: Text(
-              '\$${product.price.toStringAsFixed(2)}',
-              style: TextStyle(color: Theme.of(context).colorScheme.secondary),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(
+                '\$${product.price.toStringAsFixed(2)}',
+                style: TextStyle(color: const Color.fromARGB(255, 105, 95, 5)),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
