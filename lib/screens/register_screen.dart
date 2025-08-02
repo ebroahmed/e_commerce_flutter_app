10fe6cd2d64587fd07a20001968498dcdd1a2495
Ebrahim <ebrahimahmed804853@gmail.com>
2025-08-02 10:59:30 +0300

    register screen ui updated

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -11,35 +11,60 @@ class RegisterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(title: Text("Register")),
+      appBar: AppBar(
+        title: Text(
+          "Register",
+          style: TextStyle(color: Theme.of(context).colorScheme.primary),
+        ),
+      ),
       body: Padding(
         padding: EdgeInsets.all(16),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Container(
+              margin: EdgeInsets.only(top: 30, bottom: 20, left: 20, right: 20),
+              width: 200,
+              child: Image.asset('assets/images/ecommerce.png'),
+            ),
             TextField(
               controller: emailController,
-              decoration: InputDecoration(labelText: "Email"),
+              decoration: InputDecoration(
+                labelText: "Email",
+                labelStyle: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
             ),
             TextField(
               controller: passwordController,
-              decoration: InputDecoration(labelText: "Password"),
+              decoration: InputDecoration(
+                labelText: "Password",
+                labelStyle: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
               obscureText: true,
             ),
             SizedBox(height: 16),
             ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Theme.of(context).colorScheme.surface,
+                backgroundColor: Theme.of(context).colorScheme.primary,
+              ),
               onPressed: () async {
                 try {
                   await ref
                       .read(authRepositoryProvider)
                       .register(emailController.text, passwordController.text);
 
-                  // ✅ Logout to force login manually
+                  // Logout to force login manually
                   await ref.read(authRepositoryProvider).logout();
 
-                  // ✅ Navigate back to login screen
+                  // Navigate back to login screen
                   Navigator.pop(context);
 
-                  // Optional: Show message
+                  //  Show message
                   ScaffoldMessenger.of(context).showSnackBar(
                     SnackBar(content: Text('Account created! Please login.')),
                   );
