332a8398f4d1f26a8b87b31731a8f6c2159efd71
Ebrahim <ebrahimahmed804853@gmail.com>
2025-08-02 11:24:19 +0300

    product repository added

---
+++ lib/repositories/product_repository.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_commerce_flutter_app/models/product_model.dart';
+
+class ProductRepository {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final CollectionReference productsRef = FirebaseFirestore.instance.collection(
+    'products',
+  );
+
+  Future<List<Product>> fetchProducts() async {
+    final snapshot = await productsRef.get();
+    return snapshot.docs.map((doc) {
+      return Product.fromMap(doc.data() as Map<String, dynamic>, doc.id);
+    }).toList();
+  }
+
+  Future<void> addProduct(Product product) async {
+    await productsRef.add(product.toMap());
+  }
+
+  Future<void> deleteProduct(String id) async {
+    await productsRef.doc(id).delete();
+  }
+
+  Future<void> updateProduct(Product product) async {
+    await productsRef.doc(product.id).update(product.toMap());
+  }
+}
