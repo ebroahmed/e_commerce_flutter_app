48e535dae159e0215c9c41f9c22aa705e192c4f0
Ebrahim <ebrahimahmed804853@gmail.com>
2025-08-03 13:46:27 +0300

    product admin repository created

---
+++ lib/repositories/product_admin_repository.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ProductAdminRepository {
+  final _firestore = FirebaseFirestore.instance;
+
+  Future<void> addProduct({
+    required String title,
+    required double price,
+    required String imageUrl,
+    required String category,
+  }) async {
+    final productData = {
+      'title': title,
+      'price': price,
+      'imageUrl': imageUrl,
+      'category': category,
+      'createdAt': FieldValue.serverTimestamp(),
+    };
+
+    await _firestore.collection('products').add(productData);
+  }
+}
