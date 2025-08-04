2e6db0679a2f83e473608296922d08c123e34981
Ebrahim <ebrahimahmed804853@gmail.com>
2025-08-04 12:28:23 +0300

    gradle updated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.JavaVersion
+
 plugins {
     id("com.android.application")
     // START: FlutterFire Configuration
@@ -35,10 +37,10 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_1_8
-        targetCompatibility JavaVersion.VERSION_1_8
-        coreLibraryDesugaringEnabled true
-    }
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+    isCoreLibraryDesugaringEnabled = true
+}
 
     buildTypes {
         release {
@@ -52,7 +54,6 @@ android {
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
-
 
 flutter {
     source = "../.."
